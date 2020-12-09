38ac8d982263974c7300626d5057da2b2f268297
reveurmichael <lunde.chen@yahoo.fr>
2020-12-09 09:32:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Final Difficult Questions
+
+## Total: 30 points.
+- 20 points for moving up down right left.
+- 10 points for undo.
+
+
---
+++ src/main/java/live/MovableCell.java
@@ -0,0 +1,87 @@
+package live;
+
+import processing.core.PApplet;
+
+public class MovableCell extends PApplet {
+    // A Cell object
+    class Cell {
+        float x, y;   // x,y location
+        float w, h;   // width and height
+
+        boolean isMovableCell = false;
+
+        // Cell Constructor
+        Cell(float _x, float _y, float _w, float _h) {
+            x = _x;
+            y = _y;
+            w = _w;
+            h = _h;
+        }
+
+        void display() {
+            stroke(255);
+            if(isMovableCell)
+                fill(movableCellColor[0], movableCellColor[1], movableCellColor[2]); // Cell in RED
+            else
+                fill(200, 200, 200); // Cell in GREY
+            rect(x, y, w, h);
+        }
+    }
+
+    int movableX = 10;
+    int movableY = 10;
+
+    Cell[][] cells;
+    int[] movableCellColor = {255, 0, 0}; // Initially set to Red
+
+    // Number of columns and rows in the grid
+    int cols = 21;
+    int rows = 21;
+
+    // Width and height of each cell in the grid
+    int widthCell = 40;
+    int heightCell = 40;
+
+    public void changeMovableCellColor() {
+        movableCellColor = new int[]{ (int) random(255), (int)random(255), (int)random(255)};
+    }
+
+    public void keyPressed() {
+        switch (String.valueOf(key).toLowerCase()){
+            case "c":
+                changeMovableCellColor();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void settings() {
+        size(widthCell * cols, heightCell * rows);
+        cells = new Cell[cols][rows];
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                // Initialize each Cell object
+                cells[i][j] = new Cell(i * widthCell,j * heightCell,
+                        widthCell, heightCell);
+            }
+        }
+        cells[movableX][movableY].isMovableCell = true;
+    }
+
+    public void draw() {
+        background(0);
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                cells[i][j].display();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"MovableCell"};
+        MovableCell movableCell = new MovableCell();
+        PApplet.runSketch(processingArgs, movableCell);
+    }
+
+}
